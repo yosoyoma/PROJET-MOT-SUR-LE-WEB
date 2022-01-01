cb9ea342c0a7281f4d08afd02a31a8a3494991cd
mdjamina <m.djarfi.amina@gmail.com>
2022-01-01 21:29:33 +0100

    Corrections

--- PROGRAMMES/fonctions_v4.sh
+++ PROGRAMMES/fonctions_v4.sh
@@ -2,7 +2,14 @@
 
 COLUMNS=90
 
+REP_LOG=.
+error=0
 
+function check_error(){
+    echo $(cat $REP_LOG/command.log | wc -l)
+
+    #echo $error
+}
 
 function logInfo(){
 	DATE_LOG=$(date "+%Y-%m-%d %H:%M:%S")
@@ -12,7 +19,7 @@ function logInfo(){
 
 function logSeparateur(){
         SEPARATEUR=$@
-        for((idx=0;idx<120;idx++))
+        for((idx=0;idx<150;idx++))
         do
                 echo -ne "$SEPARATEUR"
         done
@@ -56,18 +63,6 @@ function log_success(){
 }
 
 
-# function restore(){
-	# TMP=$ETAPE
-	# unset ETAPE
-	# if [ "$TMP" != "" ]; then
-		# echo "!!ERREUR!!"|tee -a $FILE_LOG
-		# echo "!!BACKUP!!"|tee -a $FILE_LOG 
-	        # if [ ${TMP} -eq 1 ]; then
-                        # #command xxxxxx todo
-                # fi
-	# fi
-# }
-
 function log_failure(){
         DATE_LOG=$(date "+%Y-%m-%d %H:%M:%S")
         MSG="[ $DATE_LOG ] $@ ..."
@@ -86,83 +81,204 @@ function log_failure(){
 }
 
 function detection_encodage(){
-    encodage=$(curl -sIL $url | egrep charset | cut -d"=" -f2 | tr -d '\r' | tr '[a-z]' '[A-Z]' | tail -n1);
 
-    if [[ $encodage == "" ]]
+    if [[ $NOM_FIC_PA != "" ]]
     then
-        encodage=$(egrep -oi -m 1 "<meta[^>]*charset ?= ?\"?[^\" ,]+\"?" $NOM_PAGES_ASPIREE | egrep -oi -m 1 "charset.+" | cut -f2 -d= | tr -d '\r"' | tr '[a-z]' '[A-Z]')
-        #encodage=$(cat $NOM_PAGES_ASPIREE | tr '/>' '\n'| egrep -m 1 'charset=' | cut -d"=" -f2 | tr -d '\r"' | tr '[a-z]' '[A-Z]')
-    fi
+        ENCODAGE=$(curl -sIL $URL | egrep charset | cut -d"=" -f2 | tr -d '\r' | tr '[a-z]' '[A-Z]' | tail -n1);
+        
+        CHECK_ENCODAGE=0
 
-    if [[ $encodage == "" ]]
-    then
-        encodage_ok=0;
-        log_failure "Détection de l'encodage";
-    else
-        encodage_ok=1;
-        log_success "Détection de l'encodage : charset=$encodage";
-    fi
+        if [[ $ENCODAGE == "" ]]
+        then
+            ENCODAGE=$(egrep -oi -m 1 "<meta[^>]*charset ?= ?\"?[^\" ,]+\"?" $NOM_FIC_PA | egrep -oi -m 1 "charset.+" \
+            | cut -f2 -d= | tr -d '\r"' | tr '[a-z]' '[A-Z]')
+            #encodage=$(cat $NOM_FIC_PA | tr '/>' '\n'| egrep -m 1 'charset=' | cut -d"=" -f2 | tr -d '\r"' | tr '[a-z]' '[A-Z]')
+        fi
 
-    
+        #encodage=""
+
+        if [[ $ENCODAGE == "UTF-8" ]]
+        then
+            CHECK_ENCODAGE=1;
+            NOM_FIC_PA_UTF8=$NOM_FIC_PA;
+        fi
+
+        if [[ $ENCODAGE != "" ]] 
+        then
+            log_success "Détection de l'encodage : charset=$ENCODAGE" 
+        else
+            log_failure "Détection de l'encodage"
+            CHECK_ENCODAGE=-1
+        fi
+    fi
 } 
             
-
-
 function gestion_encodage(){
+    #echo $encodage | hex
+    #logInfo "check_encodage:$CHECK_ENCODAGE"
 
-            #echo $encodage | hex
+    if [[ $CHECK_ENCODAGE == 0 ]]
+    then
+        #logInfo "-----Gestion d'encodage-----"
+        
+        if [[ $(iconv -l | grep $ENCODAGE) == "" ]]
+        then
+            log_failure "L'encodage '$ENCODAGE' non supporté";
+        else
+            NOM_FIC_PA_UTF8=$(echo $NOM_FIC_PA | sed "s/.html/_UTF8.html/g") 
+            CHECK_ENCODAGE=1
 
-            if [ encodage_ok == 1 ] && [ $encodage != "UTF-8" ]
-               then
-                    
-                    if [[ $(iconv -l | grep $encodage) == "" ]]
-                    then 
-                        log_failure "L'encodage '$encodage' non supporté";
-                    else
-                        
-                        mv $NOM_PAGES_ASPIREE $encodage"_"$NOM_PAGES_ASPIREE 
-                        iconv --from $encodage --to UTF-8 $encodage"_"$NOM_PAGES_ASPIREE > $NOM_PAGES_ASPIREE && log_success "conversion UTF-8"
-                    fi
-                  
-            fi
+            iconv --from $ENCODAGE --to 0UTF-8//TRANSLIT $NOM_FIC_PA  > $NOM_FIC_PA_UTF8 2>$REP_LOG/command.log\
+                                && log_success "conversion UTF-8" || log_failure "conversion UTF-8";
 
+            [[ $(check_error) == 0 ]] || CHECK_ENCODAGE=-1
+            
+        fi
+
+        #logInfo "----------------------------"
+    fi
+
+    #logInfo $NOM_FIC_PA_UTF8
+    #logInfo $NOM_FIC_PA
             
 }
 
 
-function html_table_rows(){
+function dump_text(){
 
-    
+    if [[ $CHECK_ENCODAGE == 1 ]] && [[ $NOM_FIC_PA_UTF8 != "" ]]
+    then
+        #logInfo "--------Dump to Text--------"
+
+        NOM_FIC_DT=$REP_DT"/utf8_"$CPT_TABLE"_"$CPT_URL.txt
+
+        #dumper le contenu de la page aspirée
+        lynx -dump -nolist -assume_charset="UTF-8" -display_charset="UTF-8" $NOM_FIC_PA_UTF8 > $NOM_FIC_DT 2>$REP_LOG/command.log\
+        && log_success "dumper le contenu de la page" || log_failure "Error:"`cat $REP_LOG/command.log`;
+
+        [[ $(check_error) == 0 ]] || NOM_FIC_DT=""
+        
+        #logInfo "----------------------------"
+    fi
+}
+
+
+function extraire_contextes(){
+
+    if [[ $NOM_FIC_DT != "" ]]
+    then
+        #logInfo "--------Dump to Text--------"
+
+        NOM_FIC_CT=$REP_CT"/utf8_"$CPT_TABLE"_"$CPT_URL.txt
+
+        #dumper le contenu de la page aspirée
+                    # 3ème traitement : extraire des contextes réduits au motif (1 ligne avant et 1 ligne après)
+				# 2 méthodes : egrep + mingrep
+				# 1. construire des morceaux de corpus
+		egrep -C 2 -i "$MOTIFS" $NOM_FIC_DT > $NOM_FIC_CT 2>$REP_LOG/command.log \
+        && log_success "Extraction contextes" || log_failure "Error:"`cat $REP_LOG/command.log`;
+
+        [[ $(check_error) == 0 ]] || NOM_FIC_CT=""
+        
+        #logInfo "----------------------------"
+    fi
+}
+
+function comptage_motifs(){
+
+    if [[ $NOM_FIC_DT != "" ]]
+    then
+		COMPTEUR_MOTIFS=$(egrep -o -i $MOTIFS $NOM_FIC_DT 2>$REP_LOG/command.log | wc -l);
+
+
+        [[ $(check_error) == 0 ]] && log_success "Compteur motifs" || log_failure "Compteur motifs Error:"`cat $REP_LOG/command.log` ;
+        
+    fi
+}
+
+
+function telechargement_page(){
+
+    NOM_FIC_PA=$REP_PA/$CPT_TABLE"_"$CPT_URL.html
+
+    curl -sL -m 10 -o $NOM_FIC_PA $URL 2>$REP_LOG/command.log \
+            && log_success "Téléchargement de l'URL" || log_failure "Error:"`cat $REP_LOG/command.log` ;
+
+    [[ $(check_error) == 0 ]] || NOM_FIC_PA=""
+}
+
+function html_table_rows(){
+  
     echo "
         <tr>
-            <td align=\"center\">$cptUrl</td>
-            <td align=\"center\"><a href=\"$url\">lien n°$cptUrl</a></td>
+            <td align=\"center\">$CPT_URL</td>
+            <td align=\"center\"><a href=\"$URL\">lien n°$CPT_URL</a></td>
             <td align=\"center\">$codeHTTP</td>"
             
-
-    if [[ $NOM_PAGES_ASPIREE != "" ]]
+    if [[ $NOM_FIC_PA != "" ]]
     then 
         echo "
-            <td align=\"center\"><a href=\"$NOM_PAGES_ASPIREE\">$cptTableau"_"$cptUrl</a></td>";
+            <td align=\"center\"><a href=\"$NOM_FIC_PA\">$CPT_TABLE"_"$CPT_URL</a></td>";
     else
         echo "
             <td align=\"center\">---</td>";
     fi
     
-    
-    if [[ $encodage != "" ]]
+    if [[ $ENCODAGE != "" ]]
     then 
         echo "
-            <td align=\"center\">$encodage</td>";
+            <td align=\"center\">$ENCODAGE</td>";
+    else
+        echo "
+            <td align=\"center\">---</td>";
+    fi
+
+    if [[ $NOM_FIC_DT != "" ]]
+    then 
+        echo "
+            <td align=\"center\"><a href=\"$NOM_FIC_DT\">$CPT_TABLE"_"$CPT_URL</a></td>";
+    else
+        echo "
+            <td align=\"center\">---</td>";
+    fi
+
+    if [[ $NOM_FIC_CT != "" ]]
+    then 
+        echo "
+            <td align=\"center\"><a href=\"$NOM_FIC_CT\">$CPT_TABLE"_"$CPT_URL</a></td>";
+    else
+        echo "
+            <td align=\"center\">---</td>";
+    fi
+
+   if [[ $COMPTEUR_MOTIFS != "" ]]
+    then 
+        echo "
+            <td align=\"center\">$COMPTEUR_MOTIFS</td>";
+    else
+        echo "
+            <td align=\"center\">---</td>";
+    fi
+
+   if [[ $NOM_FIC_FW != "" ]]
+    then 
+        echo "
+            <td align=\"center\"><a href=\"$NOM_FIC_FW\">$CPT_TABLE"_"$CPT_URL</a></td>";
+    else
+        echo "
+            <td align=\"center\">---</td>";
+    fi
+
+   if [[ $NOM_FIC_BG != "" ]]
+    then 
+        echo "
+            <td align=\"center\"><a href=\"$NOM_FIC_BG\">$CPT_TABLE"_"$CPT_URL</a></td>";
     else
         echo "
             <td align=\"center\">---</td>";
     fi
     echo "    
          </tr> ";
-
-
-    
 }
 
 
@@ -206,11 +322,11 @@ function html_table(){
     
         <tr>
             <th colspan=\"11\" align=\"center\" bgcolor=\"blue\">
-                <font color=\"white\"><b>Tableau n° $cptTableau</b> <span>(Fichier: $fichier )</span></font>
+                <font color=\"white\"><b>Tableau n° $CPT_TABLE</b> <span>(Fichier: $fichier )</span></font>
             </th>
         </tr>
-        <tr><th>N°</th><th>URL</th><th>CODE HTTP</th><th>PAGE ASPIREE</th><th>Encodage</th><th>DUMP</th><th>DUMP-TEXT</th>
-        <th>Contexte</th><th>Nombre Motif</th><th>Freq.<br>des mots</th><th>Bigramme</th></tr>
+        <tr><th>N°</th><th>URL</th><th>CODE HTTP</th><th>PAGE ASPIREE</th><th>Encodage</th><th>DUMP-TEXT</th>
+        <th>Contexte</th><th>Nombre<br>Motif</th><th>Freq.<br>des mots</th><th>Bigramme</th></tr>
         "
 }
 
--- PROGRAMMES/script_s4.sh
+++ PROGRAMMES/script_s4.sh
@@ -13,15 +13,36 @@ REP_URLS=$1
 #chemin relatif du répertoire du tableau html
 TABLEAU_HTML=$2"index.html"
 
+
+MOTIFS="منع|حمل|피임|حبوب"
+
+
+
+
 # todo: valider les arguments (ou arrêter le programme)
 # il ne faut pas que les 2 variables suivantes soient "vides"
 # ET il faut qu'elles contiennent des chaines de caractères correspondant
 # à des noms de dossiers (i.e tester l'existence de ces dossier)
 
-#chemin relatif des pages aspirées
-REP_PAGES_ASPIREES=../PAGES-ASPIREES
 
-rm $REP_PAGES_ASPIREES/*.html
+
+logInfo ""
+logInfo "Motifs: $MOTIFS"
+logInfo ""
+
+logSeparateur "="
+#chemin relatif des pages aspirées
+REP_PA=../PAGES-ASPIREES
+#chemin relatif des fichiers dump
+REP_DT=../DUMP-TEXT
+
+#chemin relatif des fichiers dump
+REP_CT=../CONTEXTES
+
+#Vidange des répertoires
+rm $REP_PA/*.html >$REP_LOG/command.log 2>&1 && log_success "Purge: $REP_PA" || log_failure "Error:"`cat $REP_LOG/command.log`
+rm $REP_DT/*.txt >$REP_LOG/command.log 2>&1 && log_success "Purge: $REP_DT" || log_failure "Error:"`cat $REP_LOG/command.log`
+#rm $REP_CT/*.txt >$REP_LOG/command.log 2>&1 && log_success "Purge: $REP_CT" || log_failure "Error:"`cat $REP_LOG/command.log`
 
 #-------------------------------------------------------
 # en-tête du fichier html
@@ -33,61 +54,66 @@ html_body >>$TABLEAU_HTML
 # pour chaque élément contenu dans DOSSIER_URL
 #compteur des fichiers des urls
 # on compte les tableaux
-cptTableau=0
-for fichier in `ls $REP_URLS`;
+CPT_TABLE=0
+for FICHIER in `ls $REP_URLS`;
 do 
    logSeparateur "="
    # on compte les tableaux
-	cptTableau=$(($cptTableau + 1)) ;
+	CPT_TABLE=$(($CPT_TABLE + 1)) ;
 
    # Début de tableau
    html_table >>$TABLEAU_HTML
-
    
-   cptUrl=0
+   CPT_URL=0
 
-
-   logInfo "Traitement du fichier n°$cptTableau : "$fichier
+   logInfo "Traitement du fichier n°$CPT_TABLE : "$FICHIER
    logInfo "        "
-   while read url
+   while read URL
    do
       logSeparateur "-"
 
       #compteur des urls
-      cptUrl=$(($cptUrl+1))
+      CPT_URL=$(($CPT_URL+1))
       
-      logInfo "URL n°$cptUrl :${url:0:100}"
+      logInfo "URL n°$CPT_URL :${URL:0:100}"
       
       #le nom complet du fichier de l'url aspirée
-      NOM_PAGES_ASPIREE=""
-      encodage=""
+      NOM_FIC_PA=""
+      NOM_FIC_PA_UTF8=""
+      NOM_FIC_DT=""
+      NOM_FIC_CT=""
+      NOM_FIC_FW=""
+      NOM_FIC_BG=""
+      COMPTEUR_MOTIFS=""
+      ENCODAGE=""
+      CHECK_ENCODAGE=-1
 
       #vérification du code status http 
-      codeHTTP=$(curl -sIL -m 15 -w '%{http_code}\n' -o http_head $url | tr -d '\r\n');
+      codeHTTP=$(curl -sIL -m 15 -w '%{http_code}\n' -o http_head $URL | tr -d '\r\n');
 
+      [ $codeHTTP -eq 200 ] && log_success "Contrôle URL (HTTP CODE $codeHTTP)" || log_failure "Contrôle URL (HTTP CODE $codeHTTP)"
       
       if [[ $codeHTTP == 200 ]]
 			then 
-            log_success "Contrôle URL (HTTP CODE $codeHTTP)"
-            NOM_PAGES_ASPIREE=$REP_PAGES_ASPIREES/$cptTableau"_"$cptUrl.html
-
 
             #téléchargement (aspiration) de l'url en cours
-            curl -sL -m 10 -o $NOM_PAGES_ASPIREE $url && log_success "Téléchargement de la page"
+            telechargement_page;
 
-            #détection de l'encodage    
+            #détection de l'ENCODAGE    
             detection_encodage;
-
-            #gestion 
 
             # si encodage = UTF8 alors on fait certains traitements
 				# sinon il faudra aussi les faire et probablement d'autres
-            gestion_encodage;
+            #gestion_encodage;
 
             #
+            dump_text;
 
-         else
-            log_failure "Contrôle URL (HTTP CODE $codeHTTP)"
+            #
+            comptage_motifs;
+
+            #
+            extraire_contextes;
 
       fi
 
@@ -96,17 +122,14 @@ do
 
    logInfo "     "
 
-   done < $REP_URLS/$fichier
-
+   done < $REP_URLS/$FICHIER
    
    # Fin du tableau (et de la lecture du fichier)
    html_table_close >>$TABLEAU_HTML
-
    
 done
-      
 
 html_body_close >>$TABLEAU_HTML
 html_close>>$TABLEAU_HTML
-
+logSeparateur "="
 exit 0
