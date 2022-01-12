3d774856b7df0d5c51c0209924e2c15f098072e8
mdjamina <m.djarfi.amina@gmail.com>
2022-01-12 02:41:45 +0100

    corections

--- PROGRAMMES/concat_contexte.sh
+++ PROGRAMMES/concat_contexte.sh
@@ -4,6 +4,26 @@ REP_CT=../CONTEXTES
 
 for FICHIER in `ls $REP_CT/utf8*.txt `;
 do 
+
+    FILE_NUM=$(echo $FICHIER | cut -d"_" -f2)
+
+    echo "FICHIER=$FICHIER"
+
+    echo "FILE_NUM=$FILE_NUM"
+    echo "CONTEXTE=$REP_CT/CONTEXTES_$FILE_NUM.txt"
+    
+    cat $REP_CT"/"$FICHIER >>$REP_CT"/CONTEXTES_"$FILE_NUM".txt"
+done
+
+
+
+exit 0
+#/home/amina/Workspace/PROJET-MOT-SUR-LE-WEB/PROGRAMMES/old_scripts/concat_file.sh
+
+REP_DT=../DUMP-TEXT
+
+for FICHIER in `ls $REP_DT/utf8*.txt `;
+do 
     FILE_NUM=$(echo $FICHIER | cut -d"_" -f2)
     
     cat $REP_CT"/"$FICHIER >>$REP_CT"/CONTEXTES_"$FILE_NUM".txt"
